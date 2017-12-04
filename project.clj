0a3f34843e9fdd9efaaccee00fff347c7945fcf2
David Martin <dwmartin41@gmail.com>
2017-12-04 00:33:57 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject re-play "0.1.0-SNAPSHOT"
+  :description "Debug re-frame apps by replaying events"
+  :url "https://github.com/DaveWM/re-play"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.908" :scope "provided"]
+                 [org.clojure/core.async  "0.3.443"]
+                 [re-frame "0.10.1"]])
