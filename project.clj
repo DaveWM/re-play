824573e541c14bd700813a21306f18558a2d573d
David Martin <dwmartin41@gmail.com>
2018-03-02 11:20:54 +0000

    updated clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/DaveWM/re-play"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.908" :scope "provided"]
                  [org.clojure/core.async  "0.3.443"]
                  [re-frame "0.10.1"]])
